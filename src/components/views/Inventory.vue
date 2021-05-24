d67118c13e842f04a26dd1f6edc9cf51ff68d124
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-05-24 15:48:40 +0600

    add router-link to navigate details page.

--- src/components/views/Inventory.vue
+++ src/components/views/Inventory.vue
@@ -1,9 +1,11 @@
 <template>
     <div v-if="!loading" class="row">
         <div v-for="(item, index) in items" :key="index" class="card" style="width: 15rem;">
-            <img class="card-img-top" :src="item.photo" alt="Card image cap">
-            <div class="card-body">
+            <router-link tag="div" :to="{ path: '/item/'+item.id }">
+                <img class="card-img-top" :src="item.photo" alt="Card image cap">
                 <h5 class="card-title">{{item.title}}</h5>
+            </router-link>
+            <div class="card-body">
                 <p class="card-text">${{item.price}}</p>
                 <a @click="addToCart(item)" class="btn btn-primary">+ add</a>
             </div>
