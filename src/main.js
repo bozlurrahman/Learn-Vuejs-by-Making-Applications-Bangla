d4b72b43bbd2777a63a2cf7089aaa38bdcce68f2
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-05-25 21:03:41 +0600

    yarn add vuex

--- src/main.js
+++ src/main.js
@@ -4,21 +4,31 @@ import Vue from 'vue'
 import App from './App'
 
 window.$ = window.jQuery = require('jquery');
+
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import VueRouter from 'vue-router';
-import { routes } from "./router/index";
+
 Vue.config.productionTip = false
+
+import VueRouter from 'vue-router';
+
+import { routes } from "./router/index";
 
 Vue.use(VueRouter);
 const router = new VueRouter({
   routes
 })
 
+import Vuex from "vuex";
+Vue.use(Vuex)
+import { storage } from "./store/index";
+const store = new Vuex.Store(storage)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
